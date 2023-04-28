775bec3514709517f0979ee49218984248e0743b
Marc Koch <marckoch@users.noreply.github.com>
2023-04-28 20:57:32 +0200

    remove cloud block

--- terraform.tf
+++ terraform.tf
@@ -1,12 +1,12 @@
 terraform {
 
-  cloud {
-    organization = "organization-name"
+/*   cloud {
+    organization = "example-org-24e68a"
 
     workspaces {
       name = "learn-terraform-cloud"
     }
-  }
+  } */
 
   required_providers {
     aws = {
